10a1adc332a2cfb8674f43397be0d3f80b6f39fb
hoodakaushal <hoodakaushal@gmail.com>
2016-05-12 12:25:34 +0530

    Added company filters

--- app/src/main/java/tnpify/tnp_login/Company.java
+++ app/src/main/java/tnpify/tnp_login/Company.java
@@ -1,7 +1,10 @@
 package tnpify.tnp_login;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by Tushar on 12-05-2016.
@@ -10,6 +13,9 @@ public class Company {
     public int id;
     public String name;
     public String[] locations;
+    public String ctc; //Cost to company
+    public String cgpa; //minimum CGPA
+    public String deadline; //Application deadline.
     public Company(int id, String name, String[] locations) {
         this.id = id;
         this.name = name;
@@ -23,6 +29,71 @@ public class Company {
     public static Company getCompanyFromID(int ID) {
         return new Company(ID, "company" + ID, new String[]{"loc" + 2*ID, "loc" + 2*ID+1});
     }
+
+    public static List<Company> filterName(Company[] companies, String name){
+        List<Company> ret = new ArrayList<Company>();
+        for(int i = 0; i < companies.length; i++){
+            try {
+                if (companies[i].name.equalsIgnoreCase(name)) {
+                    ret.add(companies[i]);
+                }
+            }
+
+            catch (Exception e){
+
+            }
+        }
+        return ret;
+    }
+
+    public static List<Company> filterLocation(Company[] companies, String location){
+        List<Company> ret = new ArrayList<Company>();
+        for(int i = 0; i < companies.length; i++){
+            try {
+                if (Arrays.asList(companies[i].locations).contains(location)) {
+                    ret.add(companies[i]);
+                }
+            }
+
+            catch (Exception e){
+
+            }
+        }
+        return ret;
+    }
+
+    public static List<Company> filterCTC(Company[] companies, String ctc){
+        List<Company> ret = new ArrayList<Company>();
+        for(int i = 0; i < companies.length; i++){
+            try {
+                if (Integer.parseInt(companies[i].ctc) >= Integer.parseInt(ctc)) {
+                    ret.add(companies[i]);
+                }
+            }
+
+            catch (Exception e){
+
+            }
+        }
+        return ret;
+    }
+
+    public static List<Company> filterCGPA(Company[] companies, String cgpa){
+        List<Company> ret = new ArrayList<Company>();
+        for(int i = 0; i < companies.length; i++){
+            try {
+                if (Float.parseFloat(companies[i].cgpa) <= Float.parseFloat(cgpa)) {
+                    ret.add(companies[i]);
+                }
+            }
+
+            catch (Exception e){
+                ret.add(companies[i]);
+            }
+        }
+        return ret;
+    }
+
 
     public static ArrayList<Company> getCompaniesFromIDs(int[] IDs) {
         ArrayList<Company> list = new ArrayList<Company>(IDs.length);
